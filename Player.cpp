886fc0c2b2fe03944956ba15de3b8793dd4f607c
martinlietz <34095804+martinlietz@users.noreply.github.com>
2022-03-21 01:41:18 -0300

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,12 @@
+ #include <string>
+ 
+ using namespace std;
+ 
+ class Player
+    {
+        public:
+        string shortname;
+        int gameScore = 0;
+        bool active = true;
+        
+    };
